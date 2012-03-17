0b4088399daff5ac9bb9eaf9fe530943a160f49f
Sibi <sibi@psibi.in>
2012-03-17 13:29:50 +0530

    License headers added

--- rstep.py
+++ rstep.py
@@ -3,6 +3,8 @@
 # stepic - Python image steganography
 # Copyright (C) 2007 Lenny Domnitser
 #
+# This file is part of Rizzy.
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
